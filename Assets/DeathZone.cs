d0a3bf4e1856a419b692caf8c84a1d7c9ab6d48f
joseka1234 <joseka1234@gmail.com>
2017-06-22 15:27:39 +0200

    Implementada escena con integración de Avatar SDK y NewtonVR

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+	void OnCollisionEnter (Collision col)
+	{
+		Destroy (col.collider.gameObject);
+	}
+}
---
+++ Assets/Scripts/NewtonVR/ManagerOfScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerOfScene : MonoBehaviour
+{
+	public GameObject ball;
+	public Transform ballPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GameObject.Find ("Ball") == null) {
+			GameObject ballObj = Instantiate (ball, ballPosition.position, Quaternion.identity) as GameObject;
+			ballObj.name = "Ball";
+		}
+	}
+}
