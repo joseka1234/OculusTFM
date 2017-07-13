f1696d0aae374d943d765283de960d6cd760cf08
joseka1234 <joseka1234@gmail.com>
2017-07-13 20:14:55 +0200

    Añadidos objetos para interactuar en la escena compleja

---
+++ Assets/Scripts/Basic/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+	private const float RESET_TIME = 10f;
+	private Vector3 initialPosition;
+
+	void Start ()
+	{
+		initialPosition = transform.position;
+	}
+
+	public void ResetBall ()
+	{
+		StartCoroutine (ResetBallCoroutine ());
+	}
+
+	private  IEnumerator ResetBallCoroutine ()
+	{
+		yield return new WaitForSeconds (RESET_TIME);
+		transform.position = initialPosition;
+	}
+}
