64f24a5429ea676438c7e75f5adb892b4da1de15
joseka1234 <joseka1234@gmail.com>
2017-07-22 19:24:40 +0200

    Creada escena de naturaleza y comenzada escena jugable

---
+++ Assets/Scripts/Basic/GimmickScene/Collectable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.transform.tag == "Player") {
+			GameObject.Find ("GameManager").GetComponent<GameManager> ().IncrementScore ();
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Basic/GimmickScene/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public int Score;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void IncrementScore ()
+	{
+		Score++;
+
+	}
+}
