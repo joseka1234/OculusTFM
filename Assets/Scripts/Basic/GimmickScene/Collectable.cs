afd0c31bf14d7a80fd7208a28890a8c153ea70e6
joseka1234 <joseka1234@gmail.com>
2017-07-23 20:19:16 +0200

    Modificado script de collectable para adaptarlo a las necesidades del proyecto

--- Assets/Scripts/Basic/GimmickScene/Collectable.cs
+++ Assets/Scripts/Basic/GimmickScene/Collectable.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+
+	private const float ROTATION_INCREMENT = 1.0f;
+
+	void Update ()
+	{
+
+		transform.Rotate (new Vector3 (0, 0, ROTATION_INCREMENT));
+	}
+
 	void OnCollisionEnter (Collision col)
 	{
 		if (col.transform.tag == "Player") {
